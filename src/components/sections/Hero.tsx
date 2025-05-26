c179aea0c584a7160c3e9e5c41660731eaeed53d
Abyte.Dev <abbeyzino51@gmail.com>
2025-05-26 18:41:57 +0100

    Update Hero component with adjusted carousel

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -34,7 +34,7 @@ const Hero: React.FC<HeroProps> = ({
       zIndex: 1,
       x: 0,
       opacity: 1
-    }),
+    },
     exit: (direction: number) => ({
       zIndex: 0,
       x: direction < 0 ? 1000 : -1000,
